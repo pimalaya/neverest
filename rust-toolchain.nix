1ff6d37357729e9720bd888316a7f64d887bb688
Clément DOUIN <clement.douin@posteo.net>
2024-04-14 08:50:48 +0200

    use rust v1.77.0

--- rust-toolchain.nix
+++ rust-toolchain.nix
@@ -2,7 +2,7 @@ fenix:
 
 let
   file = ./rust-toolchain.toml;
-  sha256 = "3St/9/UKo/6lz2Kfq2VmlzHyufduALpiIKaaKX4Pq0g=";
+  sha256 = "+syqAd2kX8KVa8/U2gz3blIQTTsYYt3U63xBWaGOSc8=";
 in
 {
   fromFile = { system }: fenix.packages.${system}.fromToolchainFile {
